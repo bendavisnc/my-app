7965296d3fca6bea835cadcd86a3363ed561d57a
ben <bendavis@gmail.com>
2024-11-26 19:01:56 -0500

    Adds starting out point.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/core.async  "0.4.500"]
-                 [reagent "0.10.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]]
+                 [reagent "1.2.0"]
+                 [re-frame "1.4.3"]]  
 
   :plugins [[lein-figwheel "0.5.20"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
--- src/my_app/core.cljs
+++ src/my_app/core.cljs
@@ -1,27 +1,28 @@
 (ns my-app.core
     (:require 
-              [reagent.core :as reagent :refer [atom]]
-              [reagent.dom :as rd]))
+      [reagent.dom.client :as rdc]
+      [re-frame.core :as rf]))
+
 
 (enable-console-print!)
 
-(println "This text is printed from src/my-app/core.cljs. Go ahead and edit it and see reloading in action.")
+(def x "✘")
+(def o "⭕")
 
-;; define your app data so that it doesn't get over-written on reload
+(defn ui
+  []
+  [:div (str x o)])
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-root
+  (rdc/create-root (js/document.getElementById "app")))
 
+(defn mount-ui
+  []
+  (rdc/render app-root [ui])) ;; mount the application's ui
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+(defn run
+  []
+  (rf/dispatch-sync [:initialize])     ;; puts a value into application state
+  (mount-ui))
 
-(rd/render [hello-world]
-           (. js/document (getElementById "app")))
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(run)
