b9dd7b8afcc66011fc01fec79eab2d6f5d0f811d
bendavisnc <bendavisnc@gmail.com>
2025-04-26 15:12:43 -0400

    Adds test case for winner condition.

--- test/my_app/models/tictactoe_test.cljs
+++ test/my_app/models/tictactoe_test.cljs
@@ -27,11 +27,21 @@
      (testing "new move"
        (re-frame/dispatch-sync [:initialize])
        (re-frame/dispatch [::model/on-square-select 0])
-       (is (= {:squares ["✘" nil nil nil nil nil nil nil nil],
-               :winner nil,
-               :history [{:squares [nil nil nil nil nil nil nil nil nil]}
-                         {:squares ["✘" nil nil nil nil nil nil nil nil]}],
-               :status "Next player: ⭕"}
-              @t))
+       (is (= "Next player: ⭕"
+              (:status @t)))
+       (is (s/valid? ::spec/component @t))))))
+
+(deftest winning-move-test
+  (run-test-sync
+   (let [t (re-frame/subscribe [::model/tictactoe])]
+     (testing "winning move"
+       (re-frame/dispatch-sync [:initialize])
+       (re-frame/dispatch [::model/on-square-select 0])
+       (re-frame/dispatch [::model/on-square-select 1])
+       (re-frame/dispatch [::model/on-square-select 4])
+       (re-frame/dispatch [::model/on-square-select 2])
+       (re-frame/dispatch [::model/on-square-select 8])
+       (is (= "✘"
+              (:winner @t)))
        (is (s/valid? ::spec/component @t))))))
 
